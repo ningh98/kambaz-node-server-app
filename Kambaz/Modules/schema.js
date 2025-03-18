d5f169937371ed64de8b5b691bb529d3db1704a8
Ninghui Cai <ninghuicai@Ninghuis-MacBook-Pro.local>
2025-03-17 23:05:08 -0700

    give module uuid

--- Kambaz/Modules/schema.js
+++ Kambaz/Modules/schema.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 const schema = new mongoose.Schema(
     {
+        _id: { type:String, default: uuidv4 }, 
         name: String,
         description: String,
         course: { type: String, ref: "CourseModel" },
